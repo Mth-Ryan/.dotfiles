f3a422cad94b341c6bc6957979e12788155132c4
Mateus Ryan <mthryan@protonmail.com>
2021-08-09 00:25:38 -0300

    Galaxyline setup

--- .config/nvim/init.vim
+++ .config/nvim/init.vim
@@ -29,12 +29,12 @@ call plug#begin(s:pluginsPath)
 
 "Visual
 Plug 'Mth-Ryan/yat'  "colors
-Plug 'itchyny/lightline.vim'
 Plug 'norcalli/nvim-colorizer.lua'
 Plug 'kyazdani42/nvim-web-devicons'
 Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
 Plug 'lukas-reineke/indent-blankline.nvim'
 Plug 'yamatsum/nvim-cursorline'
+Plug 'glepnir/galaxyline.nvim', {'branch': 'main'}
 
 "Utility
 Plug 'jiangmiao/auto-pairs'
@@ -100,7 +100,8 @@ require("indent_blankline").setup {
     buftype_exclude = {"terminal"}
 }
 
--- Cursorline
+-- Galaxy Line
+require("status-line")
 
 EOF
 
@@ -233,89 +234,6 @@ set shortmess+=c
 let g:completion_enable_snippet = 'UltiSnips'
 let g:UltiSnipsJumpForwardTrigger  = "<tab>"
 let g:UltiSnipsJumpBackwardTrigger = "<s-tab>"
-
-" }}}
-
-" LightLine {{{
-
-let g:lightline = {}
-
-" Theme
-let s:bg0 = "#0B0C0E"
-let s:bg1 = "#151518"
-let s:bg2 = "#222226"
-let s:fg0 = "#D4D2CF"
-let s:fg1 = "#818284"
-let s:fg2 = "#65676a"
-let s:alt = "#fd9149"
-
-let s:p = {'normal': {}, 'inactive': {}, 'tabline': {}}
-
-let s:p.normal.left     = [ [ s:fg0, s:bg1 ], [ s:fg1, s:bg2 ] ]
-let s:p.normal.middle   = [ [ s:fg2, s:bg0 ] ]
-let s:p.normal.right    = [ [ s:fg1, s:bg1 ], [ s:fg1, s:bg2 ] ]
-
-let s:p.insert  = s:p.normal
-let s:p.replace = s:p.normal
-let s:p.visual  = s:p.normal
-
-let s:p.inactive.left   = [ [ s:bg2, s:bg0 ], [ s:bg2, s:bg0 ] ]
-let s:p.inactive.middle = [ [ s:bg2, s:bg0 ] ]
-let s:p.inactive.right  = [ [ s:bg2, s:bg0 ], [ s:bg2, s:bg0 ] ]
-
-let s:p.tabline.left    = [ [ s:fg0, s:bg0 ] ]
-let s:p.tabline.tabsel  = [ [ s:bg0, s:alt ] ]
-let s:p.tabline.middle  = [ [ s:bg2, s:bg0 ] ]
-let s:p.tabline.right   = [ [ s:fg2, s:bg1 ] ]
-
-
-let g:lightline#colorscheme#my_theme#palette = lightline#colorscheme#fill(s:p)
-
-" Components
-function! LightlineReadonly()
-  return &readonly && &filetype !=# 'help' ? '' : ''
-endfunction
-
-function! LightlineLineinfo()
-    return '並' . line('.') . ':' . virtcol('.')
-endfunction
-
-function! LightlinePercent()
-    let s:percent = line('.') * 100 / line('$')
-    return ' ' . s:percent 
-endfunction
-
-function! LightlineFugitive()
-    let s:head = fugitive#head()
-    return s:head ? ' ' . s:head : ''
-endfunction
-
-let g:lightline.component_function = {
-            \ 'fugitive': 'LightlineFugitive',
-            \ 'readonly': 'LightlineReadonly',
-            \ 'lineinfo': 'LightlineLineinfo',
-            \ 'percent': 'LightlinePercent'
-            \ }
-
-" Geral Config
-
-let g:lightline.colorscheme = "my_theme"
-
-let g:lightline.separator    = { 'left': '', 'right': '' }
-let g:lightline.subseparator = { 'left': '', 'right': '' }
-
-let g:lightline.active = { 
-            \ 'left': [ [ 'mode', 'paste' ], 
-            \           [ 'readonly', 'filename', 'modified' ],
-            \           [ 'fugitive' ] ],
-            \
-            \ 'right': [ [ 'lineinfo' ], [ 'percent' ], [ 'filetype' ] ]
-            \ }
-
-let g:lightline.inactive = {
-            \ 'left':  [  ],
-            \ 'right': [  ]
-            \ }
 
 " }}}
 
---
+++ .config/nvim/lua/status-line.lua
@@ -0,0 +1,158 @@
+local line = require('galaxyline')
+local section = line.section
+
+-- Providers
+local fileinfo = require('galaxyline.provider_fileinfo')
+local condition = require('galaxyline.condition')
+local lspclient = require('galaxyline.provider_lsp')
+
+local modes = {
+    n = 'Normal',
+    i = 'Insert',
+    c = 'Command',
+    v = 'Visual',
+    V = 'Visual',
+    [''] = 'Visual',
+    R = 'Replace',
+    t = 'Terminal'
+}
+
+local colors = {
+    bg     = '#0B0C0E',
+    fg     = '#D4D2CF',
+    fg2    = '#c3c1be',
+    bg2    = '#151518',
+    bg3    = '#222226',
+    bg4    = '#303036',
+    orange = '#fd9149',
+    cyan   = '#9BC5AF',
+}
+
+section.left[1] = {
+    FirstElement = {
+        provider = function() return '▋ ' end,
+        highlight = { colors.cyan, colors.bg }
+    },
+}
+
+section.left[2] = {
+    Mode = {
+        provider = function()
+            return modes[vim.fn.mode()] .. ' '
+        end,
+        highlight = {colors.fg, colors.bg},
+        separator = '',
+        separator_highlight = {colors.bg, colors.bg3},
+    }
+}
+
+section.left[3] = {
+    GitIcon = {
+        provider = function() return '  ' end,
+        highlight = {colors.orange, colors.bg3},
+    }
+}
+
+section.left[4] = {
+    GitBranch = {
+        condition = condition.check_git_workspace,
+        provider = 'GitBranch',
+        highlight = {colors.fg2, colors.bg3},
+    }
+}
+
+section.left[5] = {
+    GitSep = {
+        provider = function() 
+            if condition.check_git_workspace() then
+                return ' '
+            end
+            return '- '
+        end,
+        highlight = {colors.fg2, colors.bg3},
+        separator = '',
+        separator_highlight = {colors.bg3, colors.bg2},
+    }
+}
+
+section.left[6] = {
+    LspError = {
+        provider = function() return '  ' end,
+        highlight = {colors.fg2, colors.bg2},
+    }
+}
+
+section.left[7] = {
+    LspErrCount = {
+        provider = 'DiagnosticError',
+        highlight = {colors.fg2, colors.bg2},
+    }
+}
+
+section.left[8] = {
+    LspWarn = {
+        provider = function() return '  ' end,
+        highlight = {colors.fg2, colors.bg2},
+    }
+}
+
+section.left[9] = {
+    LspErrCount = {
+        provider = 'DiagnosticWarn',
+        highlight = {colors.fg2, colors.bg2},
+    }
+}
+
+section.left[10] = {
+    LspSep = {
+        provider = function() return '' end,
+        highlight = {colors.fg2, colors.bg2},
+        separator = ' ',
+        separator_highlight = {colors.bg2, colors.bg},
+    }
+}
+
+section.left[11] = {
+    FileName = {
+        provider = function()
+            local icon = fileinfo.get_file_icon()
+            local name = fileinfo.get_current_file_name()
+            return icon .. name
+        end,
+        highlight = {colors.bg4, colors.bg},
+    }
+}
+
+section.right[1] = {
+    LspName = {
+        provider = function()
+            local lspName = lspclient.get_lsp_client()
+            if lspName == 'No Active Lsp' then
+                return ''
+            end
+            return ' ' .. lspName .. ' '
+        end,
+        highlight = {colors.bg4, colors.bg},
+    }
+}
+
+section.right[2] = {
+    FileEncode = {
+        provider = function() 
+            return fileinfo.get_file_encode():lower() .. ' '
+        end,
+        highlight = {colors.fg2, colors.bg2},
+        separator = '',
+        separator_highlight = {colors.bg, colors.bg2},
+    }
+}
+
+section.right[3] = {
+    Percent = {
+        provider = 'LinePercent',
+        highlight = {colors.fg2, colors.bg3},
+        icon = '',
+        separator = ' ',
+        separator_highlight = {colors.bg2, colors.bg3},
+    }
+}
